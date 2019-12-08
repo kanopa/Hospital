cacf03bb7d92f0e7cf26623cc6dda76d75cf756b
kanopa <konoplyanka1999@gmail.com>
2019-12-08 15:20:44 +0200

    add main page without calendar

--- ClientHospital/src/app/app.module.ts
+++ ClientHospital/src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { PatientComponent } from './patient/patient.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule, MatFormFieldModule, MatInputModule, MatDatepickerModule } from '@angular/material';
+
+const appRoutes: Routes = [
+  {path: 'doctor', component: DoctorsComponent},
+  {path: 'patient', component: PatientComponent},
+  {path: 'main', component: MainComponent},
+  {path: '', redirectTo: '/main', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorsComponent,
+    PatientComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true}
+    ),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
